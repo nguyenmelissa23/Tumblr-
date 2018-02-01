8102ebf896ebd370af5edcae8db6dae6b9aec9c6
Melissa Nguyen <nguyen.melissa23@gmail.com>
2018-01-31 22:29:10 -0800

    compile error

---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Tumblr' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Tumblr
+pod 'Alamofire', '~> 4.4'
+  # Add additional dependencies
+  pod 'OAuthSwift', '~> 1.1.0'
+  pod 'OAuthSwiftAlamofire'
+  pod 'AlamofireImage' 
+
+
+  target 'TumblrTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TumblrUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Melissa Phuong Nguyen on 1/31/18.
+//  Copyright © 2018 Melissa Phuong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var imageFeed: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
