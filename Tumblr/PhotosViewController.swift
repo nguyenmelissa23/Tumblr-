3ac6ded4c7a88af558bb326aa667bc70827005fc
Jigyasaa Sood <jsood@ucsd.edu>
2018-02-02 17:35:51 -0800

    fixed runtime error, fixed image sizes, need to get scrolling function working now..

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -12,16 +12,19 @@ import AlamofireImage
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    var posts: [[String: Any]] = []
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         print(indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
         let post = posts[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
 //        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
         
     
         
@@ -43,12 +46,13 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     
-    var posts: [[String: Any]] = []
+    //var posts: [[String: Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        self.tableView.rowHeight = 200
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -60,14 +64,13 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                 print(dataDictionary)
                 
-                // TODO: Get the posts and store in posts property
-                
                 // Get the dictionary from the response key
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
                 // Store the returned array of dictionaries in our posts property
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
                 
                 // TODO: Reload the table view
+                self.tableView.reloadData()
             }
         }
         task.resume()
